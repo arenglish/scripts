4e135000b00243c0c03db22c476baf0eb09062ae
arenglish <austinenglish4@gmail.com>
2019-08-31 14:09:55 -0500

    fix

--- rename-move-photos.sh
+++ rename-move-photos.sh
@@ -136,7 +136,7 @@ fi
 if [ $target_flag -eq 1 ]; then
     echo "moving images to $TARGET"
 
-    exiftool '-directory<'$TARGET \
+    exiftool -directory=$TARGET \
     $FILETYPES \
     -r -i "$TARGET" \
     "$SOURCE"
