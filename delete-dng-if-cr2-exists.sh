934ec49d8ac8bd2e2b9c578b76085ce586c4123e
arenglish <austinenglish4@gmail.com>
2019-12-15 13:39:05 -0600

    adds dry run option

--- delete-dng-if-cr2-exists.sh
+++ delete-dng-if-cr2-exists.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+dry_run_flag=0
+
+while getopts "d" opt; do
+  case ${opt} in
+    d )
+      # dry run
+      dry_run_flag=1
+      ;;
+    \? )
+      echo "Invalid option: $OPTARG" 1>&2
+      ;;
+    : )
+      echo "Invalid option: $OPTARG requires an argument" 1>&2
+      ;;
+  esac
+done
+shift $((OPTIND -1))
 
 FILES=./*
 CR2=".cr2"
@@ -8,7 +25,12 @@ do
   if [[ $f = *$DNG ]]; then
     FILENAME="${f/$DNG/''}"
     if [[ -f "$FILENAME.cr2" ]]; then
-      echo "Found DNG that has CR2\nCR2: $FILENAME.cr2\nDNG: $f"
+      if [[ $dry_run_flag -eq 1 ]]; then
+        STRING=$"Found DNG that has CR2\nCR2: $FILENAME.cr2\nDNG: $f"
+      else
+        STRING=$"Deleting DNG that has CR2\nCR2: $FILENAME.cr2\nDNG: $f"
+      fi
+      echo $STRING
     fi
   fi
 done
