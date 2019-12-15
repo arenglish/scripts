12d6942272dc4662b2df1977535df0930ab38c47
arenglish <austinenglish4@gmail.com>
2019-12-15 14:58:07 -0600

    fixes else if syntax

--- delete-dng-if-cr2-exists.sh
+++ delete-dng-if-cr2-exists.sh
@@ -44,7 +44,7 @@ do
 
       if ( [[ ! $(stat --printf="%s" $CR2_FILE) < 10000000 ]] && [[ $dry_run_flag -eq 0 ]] ) || [[ $force_flag -eq 1 ]]; then
         rm $f
-      else if [[ $dry_run_flag -eq 1 ]]
+      eliff [[ $dry_run_flag -eq 1 ]]
         printf "\n skipped $f because it's CR2 was under 10MB"
       fi
 
