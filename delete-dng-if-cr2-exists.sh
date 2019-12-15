53c9cf80241a614e5bcf7b3bc653f91fd45a1ae4
arenglish <austinenglish4@gmail.com>
2019-12-15 13:23:11 -0600

    adds dng finder

--- delete-dng-if-cr2-exists.sh
+++ delete-dng-if-cr2-exists.sh
@@ -5,7 +5,10 @@ CR2=".cr2"
 for f in $FILES
 do
   if [[ $f = *$CR2 ]]; then
-    FILENAME="${f/$CR2/''}g"
+    FILENAME="${f/$CR2/''}"
     echo "Found CR2: $FILENAME"
+    if [[ -f "$FILENAME.dng" ]]; then
+      echo "Has DNG: $FILENAME.dng"
+    fi
   fi
 done
