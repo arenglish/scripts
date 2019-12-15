be98af8ce1210c0180819fd315f56ee3e689884e
arenglish <austinenglish4@gmail.com>
2019-12-15 17:46:18 -0600

    fixes glob

--- delete-dng-if-cr2-exists.sh
+++ delete-dng-if-cr2-exists.sh
@@ -22,33 +22,31 @@ while getopts "df" opt; do
 done
 shift $((OPTIND -1))
 
-FILES=*
+FILES="*.dng **/*.dng"
 CR2=".cr2"
 DNG=".dng"
 for f in $FILES
 do
-  if [[ $f = *$DNG ]]; then
-    FILENAME="${f/$DNG/''}"
-    CR2_FILE="$FILENAME.cr2"
-    if [[ -f $CR2_FILE ]]; then
-      if [[ $delete_flag -eq 0 ]]; then
-        MESSAGE="Found DNG and matching CR2"
-      else
-        MESSAGE="Deleting DNG that matches existing CR2"
-      fi
-      printf "\n$MESSAGE\nCR2: ($(stat --printf="%s" $CR2_FILE) bytes) $CR2_FILE\nDNG: ($(stat --printf="%s" $f) bytes) $f"
-
-      if [[ ($(stat --printf="%s" $CR2_FILE) < 10000000) ]]; then
-        printf "\nWARNING - CR2 file is smaller than 10MB, it may not be valid! Will skip deletion unless forced"
-      fi
-
-      if ( [[ ! $(stat --printf="%s" $CR2_FILE) < 10000000 ]] && [[ $delete_flag -eq 1 ]] ) || [[ $force_flag -eq 1 ]]; then
-        rm $f
-      elif [[ $delete_flag -eq 1 ]]; then
-        printf "\n skipped $f because it's CR2 was under 10MB"
-      fi
-
-      printf "\n"
+  FILENAME="${f/$DNG/''}"
+  CR2_FILE="$FILENAME.cr2"
+  if [[ -f $CR2_FILE ]]; then
+    if [[ $delete_flag -eq 0 ]]; then
+      MESSAGE="Found DNG and matching CR2"
+    else
+      MESSAGE="Deleting DNG that matches existing CR2"
     fi
+    printf "\n$MESSAGE\nCR2: ($(stat --printf="%s" $CR2_FILE) bytes) $CR2_FILE\nDNG: ($(stat --printf="%s" $f) bytes) $f"
+
+    if [[ ($(stat --printf="%s" $CR2_FILE) < 10000000) ]]; then
+      printf "\nWARNING - CR2 file is smaller than 10MB, it may not be valid! Will skip deletion unless forced"
+    fi
+
+    if ( [[ ! $(stat --printf="%s" $CR2_FILE) < 10000000 ]] && [[ $delete_flag -eq 1 ]] ) || [[ $force_flag -eq 1 ]]; then
+      rm $f
+    elif [[ $delete_flag -eq 1 ]]; then
+      printf "\n skipped $f because it's CR2 was under 10MB"
+    fi
+
+    printf "\n"
   fi
 done
